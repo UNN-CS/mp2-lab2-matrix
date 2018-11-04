10e515186935a5d10c83ad4305961fcc62ad1075
Galuzina <johndoe@example.com>
2018-11-04 18:38:17 +0300

    тесты

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -27,49 +27,21 @@ TEST(TMatrix, can_create_copied_matrix)
 TEST(TMatrix, copied_matrix_is_equal_to_source_one)
 {
 	ADD_FAILURE();
-	const int size = 2;
-	TMatrix<int> m(size);
-	for (int i = 0; i < size; i++)
-	for (int j = i; j < size; j++)
-		m[i][j] = 1;
-	TMatrix<int> mt(m);
-
-	EXPECT_EQ(m, mt);
 }
 
 TEST(TMatrix, copied_matrix_has_its_own_memory)
 {
 	ADD_FAILURE();
-	const int size = 3;
-	TMatrix<int> m(size);
-	for (int i = 0; i < size; i++)
-	for (int j = i; j < size; j++)
-		m[i][j] = 1;
-	TMatrix<int>  mt(m);
-	TMatrix<int>* p = &m;
-	TMatrix<int>* pt = &mt;
-
-	EXPECT_NE(p, pt);
 }
 
 TEST(TMatrix, can_get_size)
 {
 	ADD_FAILURE();
-	const int size = 6;
-	TMatrix<int> m(size);
-
-	EXPECT_EQ(size, m.GetSize());
 }
 
 TEST(TMatrix, can_set_and_get_element)
 {
 	ADD_FAILURE();
-	const int size = 3;
-	const int val = 4;
-	TMatrix<int> m(size);
-	m[1][2] = val;
-
-	EXPECT_EQ(val, m[1][2]);
 }
 
 TEST(TMatrix, throws_when_set_element_with_negative_index)
