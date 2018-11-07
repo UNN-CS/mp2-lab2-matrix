2593e1319cb199dd38326cb864dc264a339f053d
Alina Bykovskaya <bykovskaya.alina@gmail.com>
2018-11-07 20:01:34 +0300

    tests conpleted

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -1,6 +1,6 @@
 #include "utmatrix.h"
 
-#include "gtest.h"
+#include <gtest/gtest.h>
 
 TEST(TMatrix, can_create_matrix_with_positive_length)
 {
@@ -26,86 +26,134 @@ TEST(TMatrix, can_create_copied_matrix)
 
 TEST(TMatrix, copied_matrix_is_equal_to_source_one)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(5);
+	TMatrix<int> m2(m1);
+
+	EXPECT_EQ(m2, m1);
 }
 
 TEST(TMatrix, copied_matrix_has_its_own_memory)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(5);
+	TMatrix<int> m2(m1);
+
+	EXPECT_NE(&m1, &m2);
 }
 
 TEST(TMatrix, can_get_size)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(5);
+
+	EXPECT_EQ(5, m1.GetSize());
 }
 
 TEST(TMatrix, can_set_and_get_element)
 {
-  ADD_FAILURE();
+	TMatrix<int> m(3);
+	m[1][1] = 6;
+
+	EXPECT_EQ(6, m[1][1]);
 }
 
 TEST(TMatrix, throws_when_set_element_with_negative_index)
 {
-  ADD_FAILURE();
+	TMatrix<int> m(3);
+
+	ASSERT_ANY_THROW(m[-1][1] = 9);
 }
 
 TEST(TMatrix, throws_when_set_element_with_too_large_index)
 {
-  ADD_FAILURE();
+	TMatrix<int> m(3);
+
+	ASSERT_ANY_THROW(m[1][MAX_MATRIX_SIZE + 1] = 4);
 }
 
 TEST(TMatrix, can_assign_matrix_to_itself)
 {
-  ADD_FAILURE();
+	TMatrix<int> m(3);
+
+	ASSERT_NO_THROW(m = m);
 }
 
 TEST(TMatrix, can_assign_matrices_of_equal_size)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(3);
+	TMatrix<int> m2(3);
+
+	ASSERT_NO_THROW(m1 = m2);
 }
 
 TEST(TMatrix, assign_operator_change_matrix_size)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(3);
+	TMatrix<int> m2(5);
+
+	m2 = m1;
+
+	EXPECT_EQ(3, m2.GetSize());
 }
 
 TEST(TMatrix, can_assign_matrices_of_different_size)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(3);
+	TMatrix<int> m2(5);
+
+	ASSERT_NO_THROW(m2 = m1);
 }
 
 TEST(TMatrix, compare_equal_matrices_return_true)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(3);
+	TMatrix<int> m2(m1);
+
+	EXPECT_EQ(true, m2 == m1);
 }
 
 TEST(TMatrix, compare_matrix_with_itself_return_true)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(3);
+
+	EXPECT_EQ(true, m1 == m1);
 }
 
 TEST(TMatrix, matrices_with_different_size_are_not_equal)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(3);
+	TMatrix<int> m2(5);
+
+	EXPECT_EQ(false, m2 == m1);
 }
 
 TEST(TMatrix, can_add_matrices_with_equal_size)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(3);
+	TMatrix<int> m2(3);
+
+	ASSERT_NO_THROW(m1 + m2);
 }
 
 TEST(TMatrix, cant_add_matrices_with_not_equal_size)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(3);
+	TMatrix<int> m2(5);
+
+	ASSERT_ANY_THROW(m1 + m2);
 }
 
 TEST(TMatrix, can_subtract_matrices_with_equal_size)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(3);
+	TMatrix<int> m2(3);
+
+	ASSERT_NO_THROW(m1 - m2);
 }
 
 TEST(TMatrix, cant_subtract_matrixes_with_not_equal_size)
 {
-  ADD_FAILURE();
+	TMatrix<int> m1(3);
+	TMatrix<int> m2(5);
+
+	ASSERT_ANY_THROW(m1 - m2);
 }
 
--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -1,6 +1,6 @@
 #include "utmatrix.h"
 
-#include "gtest.h"
+#include <gtest/gtest.h>
 
 TEST(TVector, can_create_vector_with_positive_length)
 {
@@ -31,12 +31,18 @@ TEST(TVector, can_create_copied_vector)
 
 TEST(TVector, copied_vector_is_equal_to_source_one)
 {
-  ADD_FAILURE();
+	TVector<int> v(5);
+	TVector<int> exp(v);
+
+	EXPECT_EQ(v, exp);
 }
 
 TEST(TVector, copied_vector_has_its_own_memory)
 {
-  ADD_FAILURE();
+	TVector<int> v1(5);
+	TVector<int> v2(v1);
+
+	EXPECT_NE(&v1, &v2);
 }
 
 TEST(TVector, can_get_size)
@@ -63,91 +69,141 @@ TEST(TVector, can_set_and_get_element)
 
 TEST(TVector, throws_when_set_element_with_negative_index)
 {
-  ADD_FAILURE();
+	TVector<int> v(3);
+
+	ASSERT_ANY_THROW(v[-1] = 2);
 }
 
 TEST(TVector, throws_when_set_element_with_too_large_index)
 {
-  ADD_FAILURE();
+	TVector<int> v(3);
+
+	ASSERT_ANY_THROW(v[MAX_VECTOR_SIZE + 1] = 2);
 }
 
 TEST(TVector, can_assign_vector_to_itself)
 {
-  ADD_FAILURE();
+	TVector<int> v(3);
+
+	ASSERT_NO_THROW(v = v);
 }
 
 TEST(TVector, can_assign_vectors_of_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> v1(3);
+	TVector<int> v2(3);
+
+	ASSERT_NO_THROW(v1 = v2);
 }
 
 TEST(TVector, assign_operator_change_vector_size)
 {
-  ADD_FAILURE();
+	TVector<int> v1(3);
+	TVector<int> v2(5);
+
+	v2 = v1;
+
+	EXPECT_NE(5, v2.GetSize());
+
 }
 
 TEST(TVector, can_assign_vectors_of_different_size)
 {
-  ADD_FAILURE();
+	TVector<int> v1(3);
+	TVector<int> v2(5);
+
+	ASSERT_NO_THROW(v2 = v1);
 }
 
 TEST(TVector, compare_equal_vectors_return_true)
 {
-  ADD_FAILURE();
+	TVector<int> v1(3);
+	TVector<int> v2(3);
+
+	EXPECT_EQ(true, v1 == v2);
 }
 
 TEST(TVector, compare_vector_with_itself_return_true)
 {
-  ADD_FAILURE();
+	TVector<int> v(2);
+
+	ASSERT_NO_THROW(v == v);
 }
 
 TEST(TVector, vectors_with_different_size_are_not_equal)
 {
-  ADD_FAILURE();
+	TVector<int> v1(3);
+	TVector<int> v2(5);
+
+	EXPECT_EQ(false, v1 == v2);
 }
 
 TEST(TVector, can_add_scalar_to_vector)
 {
-  ADD_FAILURE();
+	TVector<int> v(3);
+
+	ASSERT_NO_THROW(v + 6);
 }
 
 TEST(TVector, can_subtract_scalar_from_vector)
 {
-  ADD_FAILURE();
+	TVector<int> v(3);
+
+	ASSERT_NO_THROW(v - 6);
 }
 
 TEST(TVector, can_multiply_scalar_by_vector)
 {
-  ADD_FAILURE();
+	TVector<int> v(3);
+
+	ASSERT_NO_THROW(v * 6);
 }
 
 TEST(TVector, can_add_vectors_with_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> v1(2);
+	TVector<int> v2(2);
+
+	ASSERT_NO_THROW(v1 + v2);
 }
 
 TEST(TVector, cant_add_vectors_with_not_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> v1(2); 
+	TVector<int> v2(3);
+
+	ASSERT_ANY_THROW(v1 + v2);
 }
 
 TEST(TVector, can_subtract_vectors_with_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> v1(2);
+	TVector<int> v2(2);
+
+	ASSERT_NO_THROW(v1 - v2);
 }
 
 TEST(TVector, cant_subtract_vectors_with_not_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> v1(2);
+	TVector<int> v2(3);
+
+	ASSERT_ANY_THROW(v1 - v2);
 }
 
 TEST(TVector, can_multiply_vectors_with_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> v1(2);
+	TVector<int> v2(2);
+
+	ASSERT_NO_THROW(v1 * v2);
 }
 
 TEST(TVector, cant_multiply_vectors_with_not_equal_size)
 {
-  ADD_FAILURE();
+	TVector<int> v1(2);
+	TVector<int> v2(3);
+
+	ASSERT_ANY_THROW(v1 * v2);
 }
 
