a9933f56eb283e23508564856fe123374b937fee
black8cherry <art8evd@gmail.com>
2018-11-19 19:51:44 +0300

    try num 5

--- include/utmatrix.h
+++ include/utmatrix.h
@@ -9,7 +9,7 @@
 #define __TMATRIX_H__
 
 #include <iostream>
-#include <xutility>
+#include <utility>
 
 using namespace std;
 
@@ -241,7 +241,7 @@ TMatrix<ValType>::TMatrix(int s): TVector<TVector<ValType> >(s)
 	else
 	{
 		for (int i = 0; i < s; i++)
-			this->pVector[i] = move(TVector<ValType>(s - i, i));
+			this->pVector[i] = TVector<ValType>(s - i, i);
 	}
 } /*-------------------------------------------------------------------------*/
 
