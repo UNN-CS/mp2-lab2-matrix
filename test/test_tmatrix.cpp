23654153b2e2169e945267f6c7c4a36cd4bc36ab
megarungle <megarungle@gmail.com>
2018-12-03 22:53:54 +0300

    Added my tests. End of work

--- test/test_tmatrix.cpp
+++ test/test_tmatrix.cpp
@@ -288,3 +288,85 @@ TEST(TMatrix, cant_subtract_matrixes_with_not_equal_size)
 
 	ASSERT_ANY_THROW(m1 = m1 - m2);
 }
+
+// my tests:
+
+TEST(TMatrix, can_add_three_matrices_with_equal_size_MY_TEST)
+{
+	int val = 5;
+	TMatrix<int> m1(val);
+	TMatrix<int> m2(val);
+	TMatrix<int> m3(val);
+	TMatrix<int> m(val);
+	for (int i = 0; i < val; ++i)
+	{
+		for (int j = i; j < val; ++j)
+		{
+			m1[i][j] = i + 1;
+			m2[i][j] = i + 2;
+			m3[i][j] = i + 3;
+			m[i][j] = 3 * i + 6;
+		}
+	}
+	m1 = m1 + m2 + m3;
+
+	EXPECT_EQ(m1, m);
+}
+
+TEST(TMatrix, can_subtract_three_matrices_with_equal_size_MY_TEST)
+{
+	int val = 5;
+	TMatrix<int> m1(val);
+	TMatrix<int> m2(val);
+	TMatrix<int> m3(val);
+	TMatrix<int> m(val);
+	for (int i = 0; i < val; ++i)
+	{
+		for (int j = i; j < val; ++j)
+		{
+			m1[i][j] = i + 1;
+			m2[i][j] = i + 2;
+			m3[i][j] = i + 3;
+			m[i][j] = -i - 4;
+		}
+	}
+	m1 = m1 - m2 - m3;
+
+	EXPECT_EQ(m1, m);
+}
+
+TEST(TMatrix, can_assign_three_matrices_of_equal_size_MY_TEST)
+{
+	int val = 5;
+	TMatrix<int> m1(val);
+	TMatrix<int> m2(val);
+	TMatrix<int> m3(val);
+	for (int i = 0; i < val; ++i)
+	{
+		for (int j = i; j < val; ++j)
+		{
+			m1[i][j] = i + 1;
+			m2[i][j] = i + 2;
+			m3[i][j] = i + 3;
+		}
+	}
+	m1 = m2 = m3;
+
+	EXPECT_EQ(m1, m2);
+	EXPECT_EQ(m2, m3);
+}
+
+TEST(TMatrix, can_create_zero_matrix_MY_TEST)
+{
+	ASSERT_NO_THROW(TMatrix<int> m(0));
+}
+
+TEST(TMatrix, cant_set_and_get_lower_matrix_elements_MY_TEST)
+{
+	TMatrix<int> m(3);
+
+	ASSERT_ANY_THROW(m[1][0] = 10);
+	ASSERT_ANY_THROW(m[2][0] = 10);
+	ASSERT_ANY_THROW(m[2][1] = 10);
+	ASSERT_ANY_THROW(int elem = m[1][0]);
+}
--- test/test_tvector.cpp
+++ test/test_tvector.cpp
@@ -317,3 +317,93 @@ TEST(TVector, cant_multiply_vectors_with_not_equal_size)
 
 	ASSERT_ANY_THROW(v1 = v1 * v2);
 }
+
+// my tests:
+
+TEST(TVector, can_add_three_vectors_with_equal_size_MY_TEST)
+{
+	int val = 5;
+	TVector<int> v1(val);
+	TVector<int> v2(val);
+	TVector<int> v3(val);
+	TVector<int> v(val);
+	for (int i = 0; i < val; ++i)
+	{
+		v1[i] = 1;
+		v2[i] = 2;
+		v3[i] = 3;
+		v[i] = 6;
+	}
+	v1 = v1 + v2 + v3;
+
+	EXPECT_EQ(v1, v);
+}
+
+TEST(TVector, can_subtract_three_vectors_with_equal_size_MY_TEST)
+{
+	int val = 5;
+	TVector<int> v1(val);
+	TVector<int> v2(val);
+	TVector<int> v3(val);
+	TVector<int> v(val);
+	for (int i = 0; i < val; ++i)
+	{
+		v1[i] = 1;
+		v2[i] = 2;
+		v3[i] = 3;
+		v[i] = -4;
+	}
+	v1 = v1 - v2 - v3;
+
+	EXPECT_EQ(v1, v);
+}
+
+TEST(TVector, can_assign_three_vectors_of_equal_size_MY_TEST)
+{
+	int val = 5;
+	TVector<int> v1(val);
+	TVector<int> v2(val);
+	TVector<int> v3(val);
+	for (int i = 0; i < val; ++i)
+	{
+		v1[i] = 1;
+		v2[i] = 2;
+		v3[i] = 3;
+	}
+	v1 = v2 = v3;
+
+	EXPECT_EQ(v1, v2);
+	EXPECT_EQ(v2, v3);
+}
+
+TEST(TVector, compare_vectors_with_equal_size_and_different_startindex_return_false_MY_TEST)
+{
+	int val = 5;
+	TVector<int> v1(val, 0);
+	TVector<int> v2(val, 1);
+	for (int i = 0; i < val; ++i)
+	{
+		v1[i] = i + 1;
+		v2[i+1] = i + 1;
+	}
+
+	EXPECT_FALSE(v1 == v2);
+}
+
+TEST(TVector, cant_create_vector_with_too_large_startindex_MY_TEST)
+{
+	ASSERT_ANY_THROW(TVector<int> v(5, MAX_VECTOR_SIZE + 1));
+}
+
+TEST(TVector, cant_set_and_get_element_with_pos_less_startindex_MY_TEST)
+{
+	TVector<int> v(5, 2);
+
+	ASSERT_ANY_THROW(v[0] = 0);
+	ASSERT_ANY_THROW(int elem = v[1]);
+}
+
+TEST(TVector, can_create_null_vector_MY_TEST)
+{
+	ASSERT_NO_THROW(TVector<int> v(0));
+}
