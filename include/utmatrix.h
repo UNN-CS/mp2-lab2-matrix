a39f6272535a785f4f3e27ce08cab24b1f9589c0
Danilishna <sms.dzer@gmail.com>
2018-12-21 05:06:58 +0300

    third edition

--- include/utmatrix.h
+++ include/utmatrix.h
@@ -20,53 +20,45 @@ template <class ValType>
 class TVector
 {
 protected:
-  ValType *pVector;
-  int Size;       // размер вектора
-  int StartIndex; // индекс первого элемента вектора
+	ValType *pVector;
+	int Size;       // размер вектора
+	int StartIndex; // индекс первого элемента вектора
 public:
-  TVector(int s = 10, int si = 0);
-  TVector(const TVector &v);                // конструктор копирования
-  ~TVector();
-  int GetSize()      { return Size;       } // размер вектора
-  int GetStartIndex(){ return StartIndex; } // индекс первого элемента
-  ValType& operator[](int pos);             // доступ
-  bool operator==(const TVector &v) const;  // сравнение
-  bool operator!=(const TVector &v) const;  // сравнение
-  TVector& operator=(const TVector &v);     // присваивание
+	TVector(int s = 10, int si = 0);
+	TVector(const TVector<ValType> &v);                // конструктор копирования
+	~TVector();
+	int GetSize() const { return this->Size; } // размер вектора
+	int GetStartIndex() const { return this->StartIndex; } // индекс первого элемента
+	ValType& operator[](int pos);             // доступ
+	bool operator==(const TVector<ValType> &v) const;  // сравнение
+	bool operator!=(const TVector<ValType> &v) const;  // сравнение
+	TVector<ValType>& operator=(const TVector<ValType> &v);     // присваивание
 
-  // скалярные операции
-  TVector  operator+(const ValType &val);   // прибавить скаляр
-  TVector  operator-(const ValType &val);   // вычесть скаляр
-  TVector  operator*(const ValType &val);   // умножить на скаляр
+	// скалярные операции
+	TVector<ValType>  operator+(const ValType &val);   // прибавить скаляр
+	TVector<ValType>  operator-(const ValType &val);   // вычесть скаляр
+	TVector<ValType>  operator*(const ValType &val);   // умножить на скаляр
 
-  // векторные операции
-  TVector  operator+(const TVector &v);     // сложение
-  TVector  operator-(const TVector &v);     // вычитание
-  ValType  operator*(const TVector &v);     // скалярное произведение
+	// векторные операции
+	TVector<ValType>  operator+(const TVector<ValType> &v);     // сложение
+	TVector<ValType>  operator-(const TVector<ValType> &v);     // вычитание
+	ValType  operator*(const TVector<ValType> &v);     // скалярное произведение
 
-  // ввод-вывод
-  friend istream& operator>>(istream &in, TVector &v)
-  {
-    for (int i = 0; i < v.Size; i++)
-      in >> v.pVector[i];
-    return in;
-  }
-  friend ostream& operator<<(ostream &out, const TVector &v)
-  {
-    for (int i = 0; i < v.Size; i++)
-      out << v.pVector[i] << ' ';
-    return out;
-  }
+	// ввод-вывод
+	friend istream& operator>>(istream &in, TVector<ValType> &v)
+	{
+		for (int i = 0; i < v.Size; i++)
+			in >> v.pVector[i];
+		return in;
+	}
+	friend ostream& operator<<(ostream &out, const TVector<ValType> &v)
+	{
+		for (int i = 0; i < v.Size; i++)
+			out << v.pVector[i] << ' ';
+		return out;
+	}
 };
 
-template <class ValType>
-TVector<ValType>::TVector(int s, int si)
-{
-	pVector = new ValType[s];
-	Size = s;
-	StartIndex = si;
-
-} /*-------------------------------------------------------------------------*/
 template <class ValType>
 TVector<ValType>::TVector(int s, int si)
 {
